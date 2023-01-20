a0f2ab0f4cfb98ed951d3c4c6821513f19fcb90c
user <user@localhost.localdomain>
2023-01-20 17:43:44 +0900

    Make function addHyphen

--- function/company.php
+++ function/company.php
@@ -1,5 +1,5 @@
 <?php
-// テーブルデータ作成バリテーション用自作ライブラリ
+// テーブルデータ作成バリテーション用ファンクション
 function insertValidate($postData)
 {
     return ($postData['name']) !== null
@@ -12,7 +12,7 @@ function insertValidate($postData)
         && ($postData['prefix']) !== null;
 }
 
-// テーブルデータ更新用バリテーション用自作ライブラリ
+// テーブルデータ更新用バリテーション用ファンクション
 function updateValidate($postData)
 {
     return ($postData['name']) !== null
@@ -22,4 +22,9 @@ function updateValidate($postData)
         && ($postData['prefecture_code']) !== null
         && ($postData['address']) !== null
         && ($postData['mail_address']) !== null;
+}
+// 郵便番号にハイフン挿入用ファンクション
+function addHyphen($postalCode)
+{
+    return  substr_replace($postalCode, '-', 3, 0);
 }
