056c1c66703131a6a8575399fa4eb647154fa048
user <user@localhost.localdomain>
2023-01-26 12:40:19 +0900

    Make new function addPrefix

--- function/quote.php
+++ function/quote.php
@@ -4,3 +4,8 @@ function thousandsSeparator($total)
 {
     return  number_format($total);
 }
+// プレフィックスに記号挿入用ファンクション
+function addPrefix($prefixCode)
+{
+    return  substr_replace($prefixCode, 'prefix-q-', 0, 0);
+}
