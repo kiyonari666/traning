e426b2bca5157543c77861246cce0b2f8c570cba
user <user@localhost.localdomain>
2023-01-26 11:33:21 +0900

    Remove some dead function

--- function/company.php
+++ function/company.php
@@ -1,28 +1,4 @@
 <?php
-// テーブルデータ作成バリテーション用ファンクション
-function insertValidate($postData)
-{
-    return ($postData['name']) !== null
-        && ($postData['manager_name']) !== null
-        && ($postData['phone_number']) !== null
-        && ($postData['postal_code']) !== null
-        && ($postData['prefecture_code']) !== null
-        && ($postData['address']) !== null
-        && ($postData['mail_address']) !== null
-        && ($postData['prefix']) !== null;
-}
-
-// テーブルデータ更新用バリテーション用ファンクション
-function updateValidate($postData)
-{
-    return ($postData['name']) !== null
-        && ($postData['manager_name']) !== null
-        && ($postData['phone_number']) !== null
-        && ($postData['postal_code']) !== null
-        && ($postData['prefecture_code']) !== null
-        && ($postData['address']) !== null
-        && ($postData['mail_address']) !== null;
-}
 // 郵便番号にハイフン挿入用ファンクション
 function addHyphen($postalCode)
 {
