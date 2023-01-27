84a625340e945af5c4198905543a7984ef0e3a82
user <user@localhost.localdomain>
2023-01-27 15:18:36 +0900

    Rename id -> companyId

--- quotations/update.php
+++ quotations/update.php
@@ -4,7 +4,7 @@ require_once('./../config/database.php');
 require_once('./../function/common.php');
 require_once('./../function/quote.php');
 
-$listPath = './list.php?id=' . $_GET['company_id'];
+$listPath = './list.php?companyId=' . $_GET['companyId'];
 
 // データ取得部
 $sql = "select * from quotations where id=:id";
@@ -28,7 +28,6 @@ if (!empty($postData)) {
         header('Location:' . $listPath);
         exit();
 }
-
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -44,7 +43,7 @@ if (!empty($postData)) {
 
         <header>
             <h1 class="headerTitle">見積編集</h1>
-            <a href="./list.php?id=<?php echo $_GET['company_id']; ?>">戻る</a>
+            <a href="./list.php?companyId=<?php echo $_GET['companyId']; ?>">戻る</a>
         </header>
 
         <div class="container">
