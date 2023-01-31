9f6ed65152caa80eaa804bb065f4a8ec9a9dfaef
user <user@localhost.localdomain>
2023-01-31 18:22:58 +0900

    Add noto

--- function/common.php
+++ function/common.php
@@ -4,3 +4,11 @@ function h($value)
 {
     return htmlspecialchars($value, ENT_QUOTES);
 }
+
+// 型指定
+//引数の前に型宣言すると、引数の型定義できる。
+//かっこの外に ): 付けて型指定すると、返り値の型定義できる
+// function h(string $mix): string
+// {
+//     return htmlspecialchars($value, ENT_QUOTES);
+// }
