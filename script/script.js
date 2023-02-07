b2c71d42ed9084415ae14b08c2ac6ec90e90347e
user <user@localhost.localdomain>
2023-02-07 19:40:25 +0900

    Add alert massage

--- script/script.js
+++ script/script.js
@@ -1,5 +1,18 @@
 const comCreateForm = document.forms.comCreateForm
 
+// comCreateForm.postal_code.addEventListener('input', event => {
+//     if(event.target.value.length === 7) {
+//         const postcode = event.target.value
+//         fetch(`https://zipcloud.ibsnet.co.jp/api/search?zipcode=${postcode}`)
+//         .then(response => response.json())
+//         .then(data => {
+//             comCreateForm.prefecture_code.value = data.results[0].prefcode
+//             comCreateForm.address.value = data.results[0].address2 + data.results[0].address3
+//         })
+//         .catch()
+//     }
+// })
+
 comCreateForm.postal_code.addEventListener('input', event => {
     if(event.target.value.length === 7) {
         const postcode = event.target.value
@@ -9,6 +22,8 @@ comCreateForm.postal_code.addEventListener('input', event => {
             comCreateForm.prefecture_code.value = data.results[0].prefcode
             comCreateForm.address.value = data.results[0].address2 + data.results[0].address3
         })
-        .catch()
+        .catch(error => {
+            alert('郵便番号を認識できませんでした\nページ再読み込み後、もう一度入力して下さい', error)
+        })
     }
 })
